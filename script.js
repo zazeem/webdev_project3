87f69009105b2e7c70c06b331864a8ca83296375
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-16 12:26:13 -0500

    added maps

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
+var map;
 function initMap() {
 	const mapEl = document.getElementById('map')
-  mapOptions = {
+	var map = new google.maps.Map(document.getElementById('map'), { 
     center: {lat: 41.8781, lng: -87.6298},
     zoom: 9,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
-  }		
-  
-	var map = new google.maps.Map(mapEl, mapOptions);	
-	
-  const marker = new google.maps.Marker({
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+  });	
+}
+  const marker1 = new google.maps.Marker({
     map: map,
-		position: { lat: 41.5643, lng: -87.8095 },
-		animation: google.maps.Animation.BOUNCE,
-		title: "Tinley/80th Ave Station",
+		position: { lat: 41.8781, lng: -87.6298 },
+		title: "Illinois Institute of Technology",
     });
 
-  const marker = new google.maps.Marker({
+  const marker2 = new google.maps.Marker({
     map: map,
 		position: { lat: 41.5643, lng: -87.8095 },
-		animation: google.maps.Animation.BOUNCE,
 		title: "Tinley/80th Ave Station",
   });
 	
