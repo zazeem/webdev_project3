55b7e1131bc1e04feb974c8619f16132e135f070
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-15 22:37:12 -0500

    adding map

--- js/script.js
+++ js/script.js
@@ -1,24 +1,7 @@
 var map;
-async function initMap() {
+function initMap() {
 	map = new google.maps.Map(document.getElementById('map')) 
-	center: {lat: -34.397, lng: 150.644};
-	center: {lat: -34.397, lng: 150.644};
-	zoom: 8;
+    center: {lat: -34.397, lng: 150.644};
+    zoom: 8;
 });	
 }
-const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
-async function routeMarkers() {
-  const map = new Map(document.getElementById('map') as HTMLElement, {
-      center: { lat: 41.5643, lng: 87.8095 },
-      zoom: 1,
-      mapId: 'DEMO_MAP_ID',
-  });
-};
-
-async function campus() {
-  const map = new Map(document.getElementById('map') as HTMLElement, {
-      center: { lat: 41.5643, lng: 87.8095 },
-      zoom: 1,
-      mapId: 'DEMO_MAP_ID',
-  });
-};
