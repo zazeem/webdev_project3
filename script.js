de975470601250b97f3037454b27c7fdfe4ac793
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-16 12:09:17 -0500

    attempting to fix maps

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-var map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 41.8781, lng: -87.6298 },
-    zoom: 10,
+	const mapEl = document.getElementById('map')
+  mapOptions = {
+    center: {lat: 41.8781, lng: -87.6298},
+    zoom: 9,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  }		
+  
+	var map = new google.maps.Map(mapEl, mapOptions);	
+	
+  const marker = new google.maps.Marker({
+    map: map,
+		position: { lat: 41.5643, lng: -87.8095 },
+		animation: google.maps.Animation.BOUNCE,
+		title: "Tinley/80th Ave Station",
+    });
+
+  const marker = new google.maps.Marker({
+    map: map,
+		position: { lat: 41.5643, lng: -87.8095 },
+		animation: google.maps.Animation.BOUNCE,
+		title: "Tinley/80th Ave Station",
   });
-};
+	
