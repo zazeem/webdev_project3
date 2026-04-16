b91639f439601d78c5b178ae089950657b727a1e
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-15 22:27:58 -0500

    fixing media queries

--- js/script.js
+++ js/script.js
@@ -4,5 +4,21 @@ async function initMap() {
 	center: {lat: -34.397, lng: 150.644};
 	center: {lat: -34.397, lng: 150.644};
 	zoom: 8;
-};	
+});	
+}
+const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+async function routeMarkers() {
+  const map = new Map(document.getElementById('map') as HTMLElement, {
+      center: { lat: 41.5643, lng: 87.8095 },
+      zoom: 1,
+      mapId: 'DEMO_MAP_ID',
+  });
+};
 
+async function campus() {
+  const map = new Map(document.getElementById('map') as HTMLElement, {
+      center: { lat: 41.5643, lng: 87.8095 },
+      zoom: 1,
+      mapId: 'DEMO_MAP_ID',
+  });
+};
