38152ae41192cd19c0c660b0cae5ae09f08ad1c1
Zainab Azeem <z.azeem3105@gmail.com>
2026-04-27 15:33:09 -0500

    added map and fixed it

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+function initMap() {
+  const el = document.getElementById("map");
+
+  const myLocation = new google.maps.LatLng(
+    41.5643,
+    -87.8095
+  );
+
+  const mapOptions = {
+    center: myLocation,
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+
+  const myMap = new google.maps.Map(el, mapOptions);
+
+  const marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.DROP,
+    icon: "star.jpg"
+  });
+  
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener("mouseover", function () {
+    infowindow.open(myMap, marker);
+  });
+}
+
 const slider = document.getElementById("slider");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-var map;
 
 console.log(slider, prev, next);
 
